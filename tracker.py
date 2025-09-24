8444241785aa7fe4aa39a6db92a2a93e60e6604b
Justin Ryan Isidro <106646031+Crific@users.noreply.github.com>
2025-09-24 15:01:59 -0700

    Updated tracker.py
    Removed logic for db connection and moved it to a separate file,
    
    Fixed comment

--- tracker.py
+++ tracker.py
@@ -1,47 +1,8 @@
-import argparse
-import sqlite3
-from pathlib import Path
+from db import init_db, get_connection  
 
-# Database file lives in the same folder as this script
-db_path = Path(__file__).resolve().parent / "finance.db"
-
-
-# ----------------------
-# Database helpers
-# ----------------------
-def get_connection():
-    """Return a SQLite connection to finance.db"""
-    conn = sqlite3.connect(db_path)
-    conn.row_factory = sqlite3.Row
-    return conn
-
-
-def init_db():
-    """Create subscriptions table if it does not exist"""
-    # TODO
-    # Open a connection to finance.db
-    with get_connection() as conn:  
-        # Create a cursor object to execute SQL commands
-        cur = conn.cursor()
-        # Run SQL to create the 'expenses' table if it doesn't already exist
-        cur.execute("""
-        CREATE TABLE IF NOT EXISTS expenses (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            email TEXT,
-            amount REAL,
-            period TEXT NOT NULL,
-            start DATE,
-            next_payment DATE,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-        );
-        """)
-
-        # Save changes to the database
-        conn.commit()
 
 def add_purchase(name, amount):
-    """Insert a new subscription into the database"""
+    """Insert a new one-time purchase into the database"""
     # TODO
     with get_connection() as conn:  
         # Create a cursor object to execute SQL commands
